a44fdeb7dc3f6aa97499319026cdb263f9cf74cf
bipinaghimire <bipinaghimire6@gmail.com>
2021-12-06 21:13:31 +0545

    SquareStar

--- src/SquareStar.java
+++ src/SquareStar.java
@@ -2,7 +2,19 @@ import java.util.Scanner;
 
 public class SquareStar {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc=new Scanner(System.in);
+
+        for(int i=0;i<6;i++)
+        {
+
+            for(int j=0;j<6;j++)
+
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
 
 
     }
