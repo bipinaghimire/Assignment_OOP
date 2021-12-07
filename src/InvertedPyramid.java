72d482bebeb125865ebc582c008fafa0e4cab103
bipinaghimire <bipinaghimire6@gmail.com>
2021-12-07 11:54:42 +0545

    Inverse pyramid pattern

--- src/InvertedPyramid.java
+++ src/InvertedPyramid.java
@@ -1,2 +1,21 @@
+import java.util.Scanner;
+
 public class InvertedPyramid {
-}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int rows;
+        System.out.print("Enter rows you want: ");
+        rows = sc.nextInt();
+        for (int i= 0; i<= rows-1; i++)
+        {
+            for (int j=0; j<=i; j++)
+            {
+                System.out.print(" ");
+            }
+            for (int k=0; k<=rows-1-i; k++)
+            {
+                System.out.print("*" + " ");
+            }
+            System.out.println();
+        }
+}}
